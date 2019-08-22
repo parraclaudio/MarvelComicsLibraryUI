5a53b2c7837eaf61586995e5a03c27e7ca9c6072
ClaudioParra <parra.claudio@hotmail.com>
2019-08-21 21:15:30 -0300

    ajustes

--- src/app/customer/customer-details/comic-data/comic-data.component.ts
+++ src/app/customer/customer-details/comic-data/comic-data.component.ts
@@ -9,8 +9,25 @@ import { Comic } from 'src/app/_interface/comic.model';
 })
 export class ComicDataComponent implements OnInit {
   @Input() public comics: Comic[];
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  getDelayedColor(date) {
+    var formatDate = new Date(date);
+    var currentDate = new Date();
+    var delayedColor = 'black';
+console.log(date);
+    console.log(currentDate);
+    if (formatDate == currentDate) {
+      delayedColor = 'yellow';
+    }
+    if (formatDate < currentDate) {
+      delayedColor = 'red';
+    }
+    return delayedColor;
+  }
+
 }
